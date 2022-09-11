bcaec386be21b3620b275c93e65c7d0ed64296fa
Val-senseisama <101377109+Val-senseisama@users.noreply.github.com>
2022-09-11 23:37:44 +0100

    Update App.js

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ const CronJob = require("cron").CronJob;
 const app = express();
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'))
 app.use('/public/', express.static('./public'));
