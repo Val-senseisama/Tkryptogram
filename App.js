3b95848f7fc6bd800e92f1baaf789e11ada9c312
Val-senseisama <101377109+Val-senseisama@users.noreply.github.com>
2022-09-13 16:10:51 +0100

    Update App.js

--- App.js
+++ App.js
@@ -229,7 +229,7 @@ app.get("/Deposit", function(req,res){
 app.get("/Withdraw", function(req, res){
     console.log(req.user);
     const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
-        https.get(url, function(response){
+        https.get(url,{credentials: 'include'}, function(response){
                 console.log(response.statusCode);
                 const resData = [];
    
